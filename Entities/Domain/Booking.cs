659e5554c65cea42cd0b57219158c13861de5381
reyesharold <reyesharold007@gmail.com>
2025-04-30 06:52:18 +0800

    added Domain Models

---
+++ Entities/Domain/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Domain
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int ClassId { get; set; } // foreignKey for Class
+        public Class Class { get; set; } // one to one -> Class
+        public Guid UserId { get; set; } // foreignKey for Member
+        public Member Member { get; set; } // one to one -> Member
+    }
+}
--- Entities/Domain/Membership.cs
+++ Entities/Domain/Membership.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace Entities.Domain
 {
     public class Membership
     {
+        [Key]
         public int MembershipId { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int DurationInDays { get; set; }
+        public ICollection<Member> Members { get; set; } // one to many -> Member
 
     }
 }
