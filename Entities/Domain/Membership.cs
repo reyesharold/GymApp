adec7c01f053da4b6b66e90712ab981b19b38532
reyesharold <reyesharold007@gmail.com>
2025-04-29 10:58:07 +0800

    added Entities Classes

---
+++ Entities/Domain/Membership.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Domain
+{
+    public class Membership
+    {
+        public int MembershipId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int DurationInDays { get; set; }
+
+    }
+}
