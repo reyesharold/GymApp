6b0d5041d28dc1b444b235388532299d4394d3a8
reyesharold <reyesharold007@gmail.com>
2025-05-02 08:54:38 +0800

    Added Common Repository

---
+++ Repositories/Common/CommonRepo.cs
@@ -0,0 +1,90 @@
+﻿using Azure;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Common
+{
+    public class CommonRepo<T> : ICommonRepo<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _db;
+
+        public CommonRepo(ApplicationDbContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+
+        public async Task<T> AddAync(T entity)
+        {
+            await _db.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T,bool>> condition,Func<IQueryable<T>,IQueryable<T>>? query = null)
+        {
+            IQueryable<T> db = _db;
+
+            if(query != null)
+            {
+                db = query(db); 
+            }
+
+            var response = await db.FirstOrDefaultAsync(condition);
+
+            return response!;
+        }
+        // query => query.Include(c => c.Sample)
+        // query(Table);
+        public async Task<ICollection<T>> GetAllAsync(Expression<Func<T, bool>>? condition = null, Func<IQueryable<T>, IQueryable<T>>? query = null)
+        {
+            IQueryable<T> db = _db;
+
+            if (query != null)
+            {
+                db = query(db);
+            }
+
+            if (condition != null)
+            {
+                db = db.Where(condition); 
+            }
+
+            var response = await db.ToListAsync();
+
+            return response;
+        }
+
+        public async Task<bool> DeleteViaIdAsync(int id)
+        {
+            var item = await _db.FindAsync(id);
+            if (item == null) { return false; }
+
+            _db.Remove(item);
+
+            return await _context.SaveChangesAsync() > 0; //SaveChangesAsync returns an integer if rows were affected
+        }
+
+        public async Task<T> UpdateAsync(T entity, params Expression<Func<T, object>>[] updateProperties)
+        {
+            _db.Attach(entity);
+
+            foreach (var property in updateProperties)
+            {
+                _context.Entry(entity).Property(property).IsModified = true;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
---
+++ Repositories/Common/ICommonRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Common
+{
+    public interface ICommonRepo<T> where T : class
+    {
+        Task<T> AddAync(T entity);
+        Task<T> GetAsync(Expression<Func<T, bool>> condition,Func<IQueryable<T>, IQueryable<T>> query);
+        Task<ICollection<T>> GetAllAsync(Expression<Func<T, bool>>? condition = null, Func<IQueryable<T>, IQueryable<T>>? query = null);
+        Task<bool> DeleteViaIdAsync(int id);
+        Task<T> UpdateAsync(T entity, params Expression<Func<T, object>>[] updateProperties);
+    
+    }
+}
